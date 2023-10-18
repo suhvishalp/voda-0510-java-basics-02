56ab8484837f359924bf256f998264dd2c8114b1
Vishal Shah <vishalshah@Vishals-MacBook-Air.local>
2023-10-18 12:39:04 +0530

    -

--- CollectionDemos/src/ListDemo2.java
+++ CollectionDemos/src/ListDemo2.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ListDemo2 {
@@ -16,14 +18,33 @@ public class ListDemo2 {
 		listEmp.add(new Employee(101, "Rohit", 2999.00));
 		listEmp.add(new Employee(105, "Onkar", 4999.00));
 
+//		Comparator<Employee> empSalComparator = new Comparator<Employee>() {
+//
+//			@Override
+//			public int compare(Employee o1, Employee o2) {
+//				// TODO Auto-generated method stub
+//				Double emp1Sal = o1.getSalary();
+//				Double emp2Sal = o2.getSalary();
+//				return emp1Sal.compareTo(emp2Sal);
+//			}
+//			
+//		};
 		
-		Employee empTobeDeleted = new Employee(101, "Rohit", 2999.00);
-		listEmp.remove(empTobeDeleted);
+		Comparator<Employee> empSalComparator = (o1, o2) -> new Double(o1.getSalary()).compareTo(new Double(o2.getSalary()));
+			
 		
+		Collections.sort(listEmp, empSalComparator);
+		System.out.println(listEmp);
+
+		
+		
+//		Employee empTobeDeleted = new Employee(101, "Rohit", 2999.00);
+//		listEmp.remove(empTobeDeleted);
+//		
 //		int index = listEmp.indexOf(empTobeDeleted);		//contains(object)   remove(object)
 //
 //		System.out.println(index);
-		System.out.println(listEmp);
+//		System.out.println(listEmp);
 	}
 
 }
---
+++ CollectionDemos/src/PredicateDemo.java
@@ -0,0 +1,50 @@
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] numsArr = {23,54,3,6,45,2,31,7,9,35, 75, 5,2,54,56,34,2,11,33};
+		
+		
+		Predicate<Integer> evenNumPredicate = (t) -> t % 2 == 0;
+		Predicate<Integer> oddNumPredicate = (t) -> t % 2 == 1;	
+		Predicate<Integer> numPredicate1 = (t) -> t > 30;	
+		
+		Predicate<Integer> combinedPredicate = evenNumPredicate.and(numPredicate1);
+			
+		
+		//show all even numbers
+//		show(numsArr, evenNumPredicate);
+		
+		
+		//show all odd numbers
+//		show(numsArr, oddNumPredicate);
+		
+		//show all numbers greater than 30
+//		show(numsArr,numPredicate1);
+		
+		//show all even numbers greater than 30
+		show(numsArr, combinedPredicate);
+		
+		
+		//show all numbers less than 30
+		
+		
+		//show all odd numbers greater than 40
+		
+		
+		
+
+	}
+	
+	public static void show(int[] arr, Predicate<Integer> predicate) {
+		
+		for(int value : arr) {
+			if(predicate.test(value))
+				System.out.println(value);
+		}
+	}
+
+}
